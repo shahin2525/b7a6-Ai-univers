3775466ec861488fc37d67cd66e7ece30fc46ada
shahin2525 <shahin451999@gmail.com>
2023-03-28 23:39:09 -0700

    add see more button and show all data

--- js/ai.js
+++ js/ai.js
@@ -37,7 +37,8 @@ const showTools = (data) => {
             <p class="m-0 p-0">${tool.published_in}</p>
             </div>
             </div>
-            <div><i class="fas fa-arrow-right text-danger" onclick="showsingleDetails('${tool.id}')"></i></div>
+            <div><i class="fas fa-arrow-right text-danger" onclick="fetchSingleDetails('${tool.id}')" data-bs-toggle="modal"
+            data-bs-target="#exampleModal"></i></div>
             
             </div>
           </div>
@@ -56,11 +57,76 @@ const showMore = () => {
     .then((data) => showTools(data.data.tools));
 };
 
-const showsingleDetails = (id) => {
+const fetchSingleDetails = (id) => {
   const URL = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
   fetch(URL)
     .then((res) => res.json())
-    .then((data) => console.log(data.data));
+    .then((data) => showSingleDetails(data.data));
+};
+
+const showSingleDetails = (data) => {
+  console.log(data);
+  document.getElementById("show-single-details").innerHTML = `
+  <div class="row row-cols-1 row-cols-md-2 g-4" >
+  <div class="col">
+
+  <div class="card h-100">
+  <img src="${data.image_link[0]}" class="card-img-top" alt="..." />
+ 
+  <div class="card-body">
+    <h5 class="card-title">features
+    </h5>
+   
+    <p class="card-title"> <ol><li>${data.features[0]}</li><li>${data.features[1]}</li><li>${data.features[2]}</li></ol></p>
+    
+    
+  
+  </div>
+  <div class="card-footer bg-transparent border-tertiary d-flex justify-content-between align-items-center">
+  <div>
+  <div><h4>${data.name}</h4></div>
+  <div class="d-flex align-items-center gap-2">
+  <i class="fas fa-calendar m-0 p-0"></i>
+  <p class="m-0 p-0">${data.published_in}</p>
+  </div>
+  </div>
+  <div><i class="fas fa-arrow-right text-danger"></i></div>
+  
+  </div>
+</div>
+ 
+  </div>
+  
+
+  <div class="col">
+  <div class="card h-100">
+  <img src="${data.image_link[0]}" class="card-img-top" alt="..." />
+ 
+  <div class="card-body">
+    <h5 class="card-title">features
+    </h5>
+   
+    <p class="card-title"> <ol><li>${data.features[0]}</li><li>${data.features[1]}</li><li>${data.features[2]}</li></ol></p>
+    
+    
+  
+  </div>
+  <div class="card-footer bg-transparent border-tertiary d-flex justify-content-between align-items-center">
+  <div>
+  <div><h4>${data.name}</h4></div>
+  <div class="d-flex align-items-center gap-2">
+  <i class="fas fa-calendar m-0 p-0"></i>
+  <p class="m-0 p-0">${data.published_in}</p>
+  </div>
+  </div>
+  <div><i class="fas fa-arrow-right text-danger"></i></div>
+  
+  </div>
+</div>
+
+  </div>
+  
+  </div>`;
 };
 
 loadTolls();
